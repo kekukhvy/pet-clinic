80b4bf4985583f13964a73491a00468014b5820e
vladyslavkekukh <vladyslavkekukh>
2022-05-15 00:32:31 +0200

    add PetTypeService and Implementation

---
+++ pet-clinic-data/src/main/java/dev/vkekukh/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package dev.vkekukh.petclinic.services;
+
+import dev.vkekukh.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/dev/vkekukh/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,33 @@
+package dev.vkekukh.petclinic.services.map;
+
+import dev.vkekukh.petclinic.model.PetType;
+import dev.vkekukh.petclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}
