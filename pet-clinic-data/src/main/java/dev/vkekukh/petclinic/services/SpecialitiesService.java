ebc61f2cf5dd2fee96ad6202f2276e1fae27b7e5
vladyslavkekukh <vladyslavkekukh>
2022-05-16 23:30:16 +0200

    add SpecialitiesServiceMap

---
+++ pet-clinic-data/src/main/java/dev/vkekukh/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package dev.vkekukh.petclinic.services;
+
+import dev.vkekukh.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/dev/vkekukh/petclinic/services/map/SpecialitiesServiceMap.java
@@ -0,0 +1,34 @@
+package dev.vkekukh.petclinic.services.map;
+
+import dev.vkekukh.petclinic.model.Speciality;
+import dev.vkekukh.petclinic.services.SpecialitiesService;
+
+import java.util.Set;
+
+public class SpecialitiesServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+}
